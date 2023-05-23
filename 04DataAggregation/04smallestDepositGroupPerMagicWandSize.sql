75a99d04fa21335554ef091adf68b68076e44e68
Miroslav Gechev <vindaloo89@gmail.com>
2023-05-23 19:17:50 +0300

    Data Aggregation Tasks

---
+++ 04DataAggregation/04smallestDepositGroupPerMagicWandSize.sql
@@ -0,0 +1,8 @@
+SELECT 
+    deposit_group
+FROM
+    wizzard_deposits
+GROUP BY deposit_group
+HAVING AVG(magic_wand_size)
+LIMIT 1
+
---
+++ 04DataAggregation/15employeesCountSalaries.sql
@@ -0,0 +1,6 @@
+SELECT 
+    COUNT(salary) AS ''
+FROM
+    employees
+WHERE
+    manager_id IS NULL
